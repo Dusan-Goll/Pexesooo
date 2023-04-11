a4276c81673bcef929120020c37bfaf65fb4ff74
Dusan-Goll <d.goll@seznam.cz>
2023-04-11 13:49:43 +0200

    refactoring Preparation of game

--- scripts/gamePrepare.js
+++ scripts/gamePrepare.js
@@ -1,114 +1,124 @@
-// get actual setting of game size
-var sizeSetting = localStorage.getItem("deckSize") || "medium";
-
-// scoreBoxes Yellow & Green
-var yellowScoreBox = document.getElementById("Yellow"),
-greenScoreBox  = document.getElementById("Green");
-
-// number of players
-var numberOfPlayers = Number(localStorage.getItem("numOfPlayers")) || 2;
-
-if (numberOfPlayers == 2) {
-    yellowScoreBox.remove();
-    greenScoreBox.remove();
-} else if (numberOfPlayers == 3) {
-    yellowScoreBox.remove();
-}
-
-// grid size
-let cardsCount;
-if (sizeSetting === "small") {
-    // var columnsCount = 4,
-    //     rowsCount    = 3;
-    cardsCount = 12;
-} else if (sizeSetting === "medium") {
-    // var columnsCount = 5,
-    //     rowsCount    = 4;
-    cardsCount = 20;
-} else if (sizeSetting === "large") {
-    // var columnsCount = 6,
-    //     rowsCount    = 5;
-    cardsCount = 30;
-}
-
-// pictures supply
-var picturesPaths = [
-    "./images/pythonLogo.svg",
-    "./images/jsLogo.svg",
-    "./images/htmlLogo.svg",
-    "./images/cssLogo.svg",
-    "./images/svgLogo.svg",
-    "./images/gitLogo.svg",
-    "./images/githubLogo.svg",
-    "./images/vscodeLogo.svg",
-    "./images/pycharmLogo.svg",
-    "./images/lodashLogo.svg",
-    "./images/mdnLogo.svg",
-    "./images/googleLogo.svg",
-    "./images/chromeLogo.svg",
-    "./images/stackoverflowLogo.svg",
-    "./images/gfgLogo.svg"
-];
-
-// shuffling
-function shufflePictures(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * i);
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+class Game {
+    constructor() {
+      this.desk = document.getElementById('desk');
+      this.scoreBoxesCollection = document.getElementsByClassName('score-box');
+      this.sizeSetting = localStorage.getItem('deckSize') || medium;
+      this.playersNumber = localStorage.getItem('numOfPlayers') || 2;
+      this.pictures = picturesPaths;
     }
-    return array;
-}
-
-// first shuffling
-shufflePictures(picturesPaths);
-
-// pictures reduction according grid size
-picturesReduced = picturesPaths.slice(0, cardsCount/2);
-
-// twice the values to make couples
-function pushTwice(element, array) {
-    array.push(element);
-    array.push(element);
-  };
-
-var pictures = Array();
-picturesReduced.forEach(path => pushTwice(path, pictures));
-
-// second shuffling (whole deck of cards)
-shufflePictures(pictures);
-
-// desk
-let desk = document.getElementById('desk');
-
-// grid construction
-// desk.style.gridTemplateColumns = `repeat(${columnsCount}, 1fr)`;
-// desk.style.gridTemplateRows = `repeat(${rowsCount}, 1fr)`;
-
-// ids
-// const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-let ids = [];
-
-// create places for cards with id (i => row, j => column)
-for (let i = 0; i < cardsCount; i++) {
+  
+    prepare() {
+      this.reducePlayers();
+      this.shuffle(this.pictures);
+      this.reduceToHalf(this.pictures);
+      this.double(this.pictures);
+      this.shuffle(this.pictures);
+      this.createDeck();
+    }
+  
+    reducePlayers() {
+      let scoreBoxes = Array.prototype.slice.call(this.scoreBoxesCollection);
+      let playersCount = this.playersNumber;
+  
+      if (playersCount === 4) {
+        return;
+      } else if (playersCount == 3) {
+        scoreBoxes.pop().remove();
+      } else if (playersCount == 2) {
+        scoreBoxes.pop().remove();
+        scoreBoxes.pop().remove();
+      }
+    }
+  
+    createDeck() {
+      let cardsCount = this.getNumberOfCards();
+      let pictures = this.pictures;
+      // this.deck = []; // ???
+  
+      for (let i = 0; i < cardsCount; i++) {
+        let card = this.createCard(i, pictures[i]);
+        this.desk.appendChild(card);
+  
+        // this.deck.push(card); // ???
+      }
+    }
+  
+    shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * i);
+          let temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+  
+      this.pictures = array;
+    }
+  
+    reduceToHalf(array) {
+      let cardsCount = this.getNumberOfCards()
+      array = array.slice(0, cardsCount/2);
+  
+      this.pictures = array;
+    }
+  
+    double(array) {
+      let doubledPictures = [];
+      array.forEach(picture => {
+        doubledPictures.push(picture);
+        doubledPictures.push(picture);
+      });
+      array = doubledPictures;
+  
+      this.pictures = array;
+    }
+  
+    getNumberOfCards() {
+      let numberOfCards;
+      let size = this.sizeSetting;
+  
+      if (size === "small") {
+        numberOfCards = 12;
+      } else if (size === "medium") {
+        numberOfCards = 20;
+      } else if (size === "large") {
+        numberOfCards = 30;
+      }
+  
+      return numberOfCards;
+    }
     
-    let buttonElem = document.createElement('button');
-    let imgElem    = document.createElement('img');
-
-    buttonElem.setAttribute("id", `card-${i}`);
-    buttonElem.setAttribute("onclick", "turnOverCard(this)");
-
-    imgElem.setAttribute("src", "");
-    imgElem.setAttribute("alt", "");
-
-    buttonElem.appendChild(imgElem);
-    desk.appendChild(buttonElem);
-
-    ids.push(`card-${i}`);
-    
-}
-
-// bind IDs & pictures together
-var cards = {};
-ids.forEach((id, i) => cards[id] = pictures[i]);
+    createCard(i, src) {
+      let card = document.createElement('button');
+      card.setAttribute('id', `card-${i}`);
+      card.setAttribute('class', 'card');
+  
+      let front = document.createElement('div');
+      front.setAttribute('class', 'card-front');
+  
+      let back = document.createElement('img');
+      back.setAttribute('class', 'card-back');
+      back.setAttribute('src', src);
+      back.setAttribute('style', 'display: none;');
+  
+      card.appendChild(front);
+      card.appendChild(back);
+      card.addEventListener('click', () => this.vacantize(card));
+  
+      return card;
+    }
+  
+    turnOverCard() {
+      // some code
+    }
+  
+    vacantize(elem) {
+      let vacant = document.createElement('div');
+      vacant.setAttribute('class', 'vacant');
+      elem.replaceWith(vacant);
+    }
+  }
+  
+  let game = new Game();
+  
+  game.prepare();
+  
---
+++ scripts/picturesPaths.js
@@ -0,0 +1,19 @@
+const basePath = './images'
+
+const picturesPaths = [
+  `${basePath}/pythonLogo.svg`,
+  `${basePath}/jsLogo.svg`,
+  `${basePath}/htmlLogo.svg`,
+  `${basePath}/cssLogo.svg`,
+  `${basePath}/svgLogo.svg`,
+  `${basePath}/gitLogo.svg`,
+  `${basePath}/githubLogo.svg`,
+  `${basePath}/vscodeLogo.svg`,
+  `${basePath}/pycharmLogo.svg`,
+  `${basePath}/lodashLogo.svg`,
+  `${basePath}/mdnLogo.svg`,
+  `${basePath}/googleLogo.svg`,
+  `${basePath}/chromeLogo.svg`,
+  `${basePath}/stackoverflowLogo.svg`,
+  `${basePath}/gfgLogo.svg`,
+];
